c2c38d56ae5c0a1b9af6ee31ba0c8385abaea8af
Sahana Koli <sahanasultanakoli@gmail.com>
2023-09-13 11:57:17 +0600

    create layout for the blogs

--- src/component/Blogs/Blogs.jsx
+++ src/component/Blogs/Blogs.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() =>{
+        fetch('blog.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    }, [])
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+           <h1 className="text-4xl">Blogs: {blogs.length}</h1> 
         </div>
     );
 };
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,7 +1,9 @@
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-4xl text-center font-bold'>Knowledge Cafe</h1>
+        <div className='flex justify-between p-4 border-b-2'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+           <img src={profile}/>
         </div>
     );
 };
