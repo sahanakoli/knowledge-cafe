2e558dc2bde335e8ad31a71296bb88c704896820
Sahana Koli <sahanasultanakoli@gmail.com>
2023-09-13 11:04:00 +0600

    added blogs fake data using chatGPT

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Blogs from './component/Blogs/Blogs'
+import Header from './component/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-4xl text-center font-bold'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
